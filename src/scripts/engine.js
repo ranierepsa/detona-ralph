d4c5c3a0c9b2e3fa34ad6225d4fda2e2dbb089ba
ranierearaujo <ranierepsa@gmail.com>
2023-11-02 22:21:05 -0300

    feat: Add foe randomizer and making the behaviour of the timeLeft, Score and Lives

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,50 +2,97 @@ state = {
     views: {
         timeLeft: document.getElementById('header-time-value'),
         score: document.getElementById('header-score-value'),
+        lives: document.getElementById('header-life-value'),
         squares: document.querySelectorAll(".square")
     },
     values: {
         timeLeft: 60,
         score: 0,
-        respawnRate: 3000
+        respawnRate: 3000,
+        lives: 3
     },
     timers: {
-        respawn: null
+        respawn: null,
+        timeLeft: null
     }
 }
 
 function spawn() {
-    // clear animals
-    // randomize another position for foe and friend
-    // spawn foe and friend in new position
+    const spawnPosition = Math.floor(Math.random() * 9);
+
+    document.getElementsByClassName('foe')[0].remove();
+
+    const div = document.createElement('div');
+    div.className = 'foe';
+    document.getElementById(spawnPosition).appendChild(div);
 }
 
-function onclick() {
-    // if click empty spot or snake, remove a life
-    // if click a friendly animal, add a point to score
-    // if click any animal (friendly or foe), 
-        // clearAnimals()
-        // resetRespawnTimers()
+function respawn() {
+    resetSpawnTimer();
+    spawn();
 }
 
-function addClickEventToSquares() {
-    state.views.squares.forEach(elem => elem.addEventListener());
-    document.querySelectorAll()
+function containsFoe(square) {
+    return (square.matches('.foe')) ||
+        (square.firstElementChild && square.firstElementChild.matches('.foe'));
+}
 
+function checkGameOver() {
+    if (state.values.timeLeft <= 0 || state.values.lives <= 0){
+        //game over;
+        //parar timers;
+        // mostrar botao de retry;
+    }
+}
+
+function increaseScore() {
+    state.values.score++;
+    state.views.score.innerHTML = state.values.score;
+}
+
+function decreaseLives() {
+    state.values.lives--;
+    state.views.lives.innerHTML = 'x' + state.values.lives;
+    checkGameOver();
+}
+
+function decreaseTimeLeft() {
+    state.values.timeLeft--;
+    state.views.timeLeft.innerHTML = state.values.timeLeft;
+    checkGameOver();
+}
+
+function onClickSquare(htmlElement) {
+    let square = htmlElement.target;
+    if (containsFoe(square)) {
+        increaseScore();
+        respawn();
+    } else {
+        decreaseLives();
+    }
+}
+
+function addClickEventToAllSquares() {
+    state.views.squares.forEach(htmlElement => htmlElement.addEventListener('click', onClickSquare, htmlElement));
 }
 
 function initSpawnTimer() {
     state.timers.respawn = setInterval(spawn, state.values.respawnRate);
 }
 
-function restartTimers() {
+function initLeftTimer() {
+    state.timers.timeLeft = setInterval(decreaseTimeLeft, 1000);
+}
+
+function resetSpawnTimer() {
     clearInterval(state.timers.respawn);
     initSpawnTimer()
 }
 
 function startGame() {
-    addClickEventToSquares();
+    addClickEventToAllSquares();
     initSpawnTimer();
+    initLeftTimer();
     spawn();
 }
 
